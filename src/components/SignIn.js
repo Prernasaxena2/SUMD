9f60eb097051fedf73b0780a6c612309f9bbe41f
Prernacoder <prernasaxena2@gmail.com>
2023-12-22 11:05:12 +0530

    first commit

---
+++ src/components/SignIn.js
@@ -0,0 +1,100 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
+import AuthService from '../services/AuthService';
+import { login } from '../authSlice';
+
+const SignIn = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const handleSignIn = async () => {
+        try {
+            // Call the signIn method from the AuthService
+            const user = await AuthService.signIn(email, password);
+
+            if (!user) {
+                // Handle the case where the user is not found or login is unsuccessful
+                setError('User not found. Please check your credentials.');
+                return;
+            }
+
+            // Dispatch the login action to update the auth state
+            dispatch(login(user));
+
+            // Clear form fields after successful sign-in
+            setEmail('');
+            setPassword('');
+            setError('');
+
+            console.log('User signed in:', user);
+
+            // Redirect to the dashboard or any other page after successful sign-in
+            navigate('/dashboard');
+        } catch (error) {
+            // Handle sign-in error
+            setError(error.message);
+        }
+    };
+
+    return (
+        <div className="max-w-md mx-auto mt-8 p-6 bg-white rounded-md shadow-md">
+            <h2 className="text-2xl font-semibold mb-6">Sign In</h2>
+            <form>
+                <div className="mb-4">
+                    <label htmlFor="email" className="block text-gray-600 text-sm font-medium mb-2">
+                        Email:
+                    </label>
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500"
+                        required
+                    />
+                </div>
+                <div className="mb-6">
+                    <label htmlFor="password" className="block text-gray-600 text-sm font-medium mb-2">
+                        Password:
+                    </label>
+                    <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500"
+                        required
+                    />
+                </div>
+                <div className="flex items-center justify-between">
+                    <button
+                        type="button"
+                        onClick={handleSignIn}
+                        className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300"
+                    >
+                        Sign In
+                    </button>
+                </div>
+                {error && (
+                    <p className="text-red-500 text-sm mt-3">
+                        {error}
+                    </p>
+                )}
+                <p className="text-gray-600 mt-4">
+                    Don't have an account?{' '}
+                    <Link to="/signup" className="text-blue-500">
+                        Sign up here.
+                    </Link>
+                </p>
+            </form>
+        </div>
+    );
+};
+
+export default SignIn;
